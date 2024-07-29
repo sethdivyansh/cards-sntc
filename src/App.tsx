dd9985aa1037f9fc5a348cc80256c9da18e7b8ca
Sagnik Mandal <acriticalcynic@outlook.com>
2024-07-30 02:19:47 +0530

    Add Logos

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import React, { useState, useRef, useMemo, RefObject } from 'react';
-import TinderCard from 'react-tinder-card';
-import CopsCard from './components/cops';
-import cops from './assets/cops.svg';
-import aero from './assets/aero.svg';
-import tqc from './assets/tqc.svg';
-import robotics from './assets/robotics.svg';
-import csi from './assets/csi.svg';
-import bizclub from './assets/bizclub.svg';
-import astro from './assets/astro.svg';
-import { FaUndo } from 'react-icons/fa';
-import './App.css';
+import React, { useState, useRef, useMemo, RefObject } from 'react'
+import TinderCard from 'react-tinder-card'
+import CopsCard from './components/cops'
+import cops from './assets/cops.svg'
+import aero from './assets/aero.svg'
+import tqc from './assets/tqc.svg'
+import robotics from './assets/robotics.svg'
+import csi from './assets/csi.svg'
+import bizclub from './assets/bizclub.svg'
+import astro from './assets/astro.svg'
+import { FaUndo } from 'react-icons/fa'
+import './App.css'
 
 type Color =
   | 'black'
@@ -21,21 +21,21 @@ type Color =
   | 'lightPurple'
   | 'lightRed'
   | 'darkPurple'
-  | 'darkRed';
+  | 'darkRed'
 
 interface Club {
-  mainImage: string;
-  clubName: string;
-  content: string;
-  color: Color;
-  whatsappLink: string;
-  linkedinLink: string;
-  instaLink: string;
+  mainImage: string
+  clubName: string
+  content: string
+  color: Color
+  whatsappLink: string
+  linkedinLink: string
+  instaLink: string
 }
 
 interface TinderCardAPI {
-  swipe: (direction: string) => void;
-  restoreCard: () => void;
+  swipe: (direction: string) => void
+  restoreCard: () => void
 }
 
 const db: Club[] = [
@@ -47,7 +47,8 @@ const db: Club[] = [
     color: 'black',
     whatsappLink: '',
     linkedinLink: 'https://www.linkedin.com/company/cops-iitbhu/',
-    instaLink: 'https://www.instagram.com/cops.iitbhu?igsh=MXRvdjBpMWRkdmRkeA==',
+    instaLink:
+      'https://www.instagram.com/cops.iitbhu?igsh=MXRvdjBpMWRkdmRkeA==',
   },
   {
     mainImage: tqc,
@@ -56,18 +57,22 @@ const db: Club[] = [
       'The Quant Club at IIT (BHU), Varanasi, promotes quantitative finance and analytics through workshops, seminars, competitions, and projects. We offer learning, networking, and hands-on experience in algorithmic trading, risk management, and data analytics. Join us to excel in this field.',
     color: 'purple',
     whatsappLink: 'https://chat.whatsapp.com/JwehHeWwlc80n8OVXZy4LL',
-    linkedinLink: 'https://www.linkedin.com/company/the-quant-club-iit-bhu-varanasi/',
-    instaLink: 'https://www.instagram.com/quantclub.iitbhu?igsh=eDIzaWd2YzM0dWpi',
+    linkedinLink:
+      'https://www.linkedin.com/company/the-quant-club-iit-bhu-varanasi/',
+    instaLink:
+      'https://www.instagram.com/quantclub.iitbhu?igsh=eDIzaWd2YzM0dWpi',
   },
   {
     mainImage: robotics,
     clubName: 'Robotics Club',
     content:
-      'Our Robotics Club at IIT BHU empowers students with essential skills in robotics through hands-on projects, workshops, guest lectures, and competitions. Whether you\'re a beginner or expert, join us to learn, grow, and launch a successful robotics career.',
+      "Our Robotics Club at IIT BHU empowers students with essential skills in robotics through hands-on projects, workshops, guest lectures, and competitions. Whether you're a beginner or expert, join us to learn, grow, and launch a successful robotics career.",
     color: 'skyBlue',
     whatsappLink: 'https://chat.whatsapp.com/CSGHZY6xmlCIQEro82i1tz',
-    linkedinLink: 'https://www.linkedin.com/company/robotics-club-iit-bhu-varanasi/',
-    instaLink: 'https://www.instagram.com/robotics_club.iitbhu?igsh=MXFhYTdtZms0MnZscQ==',
+    linkedinLink:
+      'https://www.linkedin.com/company/robotics-club-iit-bhu-varanasi/',
+    instaLink:
+      'https://www.instagram.com/robotics_club.iitbhu?igsh=MXFhYTdtZms0MnZscQ==',
   },
   {
     mainImage: aero,
@@ -97,7 +102,8 @@ const db: Club[] = [
     color: 'blue',
     whatsappLink: 'https://chat.whatsapp.com/Iix3yxoYqUM0kMkQun9M4s',
     linkedinLink: 'https://www.linkedin.com/company/businessclubiitbhu/',
-    instaLink: 'https://www.instagram.com/businessclub_iitbhu?igsh=Mmg3eGZldW1ia2Rs',
+    instaLink:
+      'https://www.instagram.com/businessclub_iitbhu?igsh=Mmg3eGZldW1ia2Rs',
   },
   {
     mainImage: astro,
@@ -109,35 +115,38 @@ const db: Club[] = [
     linkedinLink: 'https://www.linkedin.com/company/astroiitbhu/',
     instaLink: 'https://www.instagram.com/astro.iitbhu?igsh=a2s0dGc5aG9naTh2',
   },
-];
+]
 
 function App() {
-  const [currentIndex, setCurrentIndex] = useState<number>(db.length - 1);
-  const currentIndexRef = useRef<number>(currentIndex);
+  const [currentIndex, setCurrentIndex] = useState<number>(db.length - 1)
+  const currentIndexRef = useRef<number>(currentIndex)
 
   const childRefs: RefObject<TinderCardAPI>[] = useMemo(
-    () => Array(db.length).fill(null).map(() => React.createRef<TinderCardAPI>()),
+    () =>
+      Array(db.length)
+        .fill(null)
+        .map(() => React.createRef<TinderCardAPI>()),
     [db.length]
-  );
+  )
 
   const updateCurrentIndex = (val: number) => {
-    setCurrentIndex(val);
-    currentIndexRef.current = val;
-  };
+    setCurrentIndex(val)
+    currentIndexRef.current = val
+  }
 
-  const canGoBack = currentIndex < db.length - 1;
+  const canGoBack = currentIndex < db.length - 1
   // const canSwipe = currentIndex >= 0;
 
   const swiped = (direction: string, index: number) => {
-    updateCurrentIndex(index - 1);
-    console.log('Swiped: ', direction, index);
-  };
+    updateCurrentIndex(index - 1)
+    console.log('Swiped: ', direction, index)
+  }
 
   const outOfFrame = (idx: number) => {
     if (currentIndexRef.current >= idx) {
-      childRefs[idx].current?.restoreCard();
+      childRefs[idx].current?.restoreCard()
     }
-  };
+  }
 
   // const swipe = async (dir: string) => {
   //   if (canSwipe && currentIndex >= 0) {
@@ -145,21 +154,23 @@ function App() {
   //   }
   // };
 
-  const goBack =  () => {
-    if (!canGoBack) return;
-    const newIndex = currentIndex + 1;
-    updateCurrentIndex(newIndex);
-    childRefs[newIndex].current?.restoreCard();
-  };
+  const goBack = () => {
+    if (!canGoBack) return
+    const newIndex = currentIndex + 1
+    updateCurrentIndex(newIndex)
+    childRefs[newIndex].current?.restoreCard()
+  }
 
   return (
-    <div className="cards-container">
+    <div className='cards-container'>
       <link
-        href="https://fonts.googleapis.com/css?family=Damion&display=swap"
-        rel="stylesheet"
+        href='https://fonts.googleapis.com/css?family=Damion&display=swap'
+        rel='stylesheet'
       />
-      <div className='mb-8'><h1 className="welcome-text">Welcome TO IITBHU</h1></div>
-      <div className="cardContainer">
+      <div className='mb-8'>
+        <h1 className='welcome-text'>Welcome TO IITBHU</h1>
+      </div>
+      <div className='cardContainer'>
         {db.map((club, index) => (
           <TinderCard
             ref={childRefs[index]}
@@ -180,13 +191,13 @@ function App() {
           </TinderCard>
         ))}
       </div>
-      <div className="buttons">
-        <button onClick={() => goBack()} className="undo-button">
+      <div className='buttons'>
+        <button onClick={() => goBack()} className='undo-button'>
           <FaUndo size={20} />
         </button>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
