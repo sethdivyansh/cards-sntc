79e1414c4a1f8a22577dd3894afa32edb8dee35e
aayush0325 <ayush25khanna@gmail.com>
2024-07-29 22:25:46 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import CopsCard from './components/cops'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <CopsCard/>
+    </>
+  )
+}
+
+export default App
