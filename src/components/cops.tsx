b55afb88141e56e289af3cea7a6f91d817680396
Sagnik Mandal <acriticalcynic@outlook.com>
2024-07-30 22:41:21 +0530

    Fix Whatsapp Issue

--- src/components/cops.tsx
+++ src/components/cops.tsx
@@ -65,13 +65,13 @@ const CopsCard: React.FC<CopsCardProps> = ({
       <h1 className={`text-2xl font-bold mb-4 text-${color}`}>{clubName}</h1>
       <p className='text-gray-700 mb-4 text-sm'>{content}</p>
       <div className='flex justify-center align-middle'>
-        <div
-          className={`${colorVariantsWhatsapp[color]} rounded-3xl text-white flex justify-center items-center p-2 my-3 w-1/2 font-semibold shadow-md transition-transform transform hover:scale-105 cursor-pointer`}
-          onClick={() => window.location.href = whatsappLink}
+        <a
+          className={`${colorVariantsWhatsapp[color]} pressable rounded-3xl text-white flex justify-center items-center p-2 my-3 w-1/2 font-semibold shadow-md transform hover:scale-105 cursor-pointer transition-all`}
+          href={whatsappLink}
         >
           <FaWhatsapp size={24} className='mr-2' />
-          <div onClick={() => window.location.href = whatsappLink}>WhatsApp</div>
-        </div>
+          <div>WhatsApp</div>
+        </a>
       </div>
       <div className='flex justify-center space-x-4 mt-1'>
         <a
