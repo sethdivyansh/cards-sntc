1e3d87b2cfd0330953bdc9b7811e07056cc10725
Divyansh Seth <divyanshseth08@gmail.com>
2024-07-31 00:24:11 +0530

    fix/size

--- src/App.tsx
+++ src/App.tsx
@@ -1,175 +1,176 @@
-import React, { useState, useRef, useMemo, RefObject } from "react";
-import TinderCard from "react-tinder-card";
-import CopsCard from "./components/cops";
-import cops from "./assets/cops.svg";
-import aero from "./assets/aero.svg";
-import tqc from "./assets/tqc.svg";
-import robotics from "./assets/robotics.svg";
-import csi from "./assets/csi.svg";
-import bizclub from "./assets/bizclub.svg";
-import astro from "./assets/astro.svg";
-import sae from "./assets/sae.svg";
-import { FaUndo } from "react-icons/fa";
-import "./index.css";
-import { SignedIn, SignedOut, UserButton } from "@clerk/clerk-react";
-import SignInPage from "./components/signIn";
+import React, { useState, useRef, useMemo, RefObject } from 'react'
+// import { Link } from 'react-router-dom'
+import TinderCard from 'react-tinder-card'
+import CopsCard from './components/cops'
+import cops from './assets/cops.svg'
+import aero from './assets/aero.svg'
+import tqc from './assets/tqc.svg'
+import robotics from './assets/robotics.svg'
+import csi from './assets/csi.svg'
+import bizclub from './assets/bizclub.svg'
+import astro from './assets/astro.svg'
+import sae from './assets/sae.svg'
+import { FaUndo } from 'react-icons/fa'
+import './index.css'
+import { SignedIn, SignedOut, UserButton } from '@clerk/clerk-react'
+import SignInPage from './components/signIn'
 
 type Color =
-  | "black"
-  | "purple"
-  | "skyBlue"
-  | "red"
-  | "orange"
-  | "blue"
-  | "lightPurple"
-  | "lightRed"
-  | "darkPurple"
-  | "darkRed";
+  | 'black'
+  | 'purple'
+  | 'skyBlue'
+  | 'red'
+  | 'orange'
+  | 'blue'
+  | 'lightPurple'
+  | 'lightRed'
+  | 'darkPurple'
+  | 'darkRed'
 
 interface Club {
-  mainImage: string;
-  clubName: string;
-  content: string;
-  color: Color;
-  whatsappLink: string;
-  linkedinLink: string;
-  instaLink: string;
+  mainImage: string
+  clubName: string
+  content: string
+  color: Color
+  whatsappLink: string
+  linkedinLink: string
+  instaLink: string
 }
 
-type Direction = "left" | "right" | "up" | "down";
+type Direction = 'left' | 'right' | 'up' | 'down'
 
 interface TinderCardAPI {
-  swipe: (dir: Direction) => Promise<void>;
-  restoreCard: () => Promise<void>;
+  swipe: (dir: Direction) => Promise<void>
+  restoreCard: () => Promise<void>
 }
 
 const db: Club[] = [
   {
     mainImage: cops,
-    clubName: "Club Of Programmers",
+    clubName: 'Club Of Programmers',
     content:
-      "COPS at IIT (BHU), Varanasi, fosters programming excellence through workshops, competitions, hackathons, and projects. We empower members with skills and knowledge, nurturing creativity and problem-solving. Join us to grow, connect, and excel in technology.",
-    color: "black",
-    whatsappLink: "https://chat.whatsapp.com/CF854bvOCpAEp3elZi8sOV",
-    linkedinLink: "https://www.linkedin.com/company/cops-iitbhu/",
+      'Fosters programming excellence through workshops, competitions, hackathons, and projects. Join us to grow, connect, and excel in technology.',
+    color: 'black',
+    whatsappLink: 'https://chat.whatsapp.com/CF854bvOCpAEp3elZi8sOV',
+    linkedinLink: 'https://www.linkedin.com/company/cops-iitbhu/',
     instaLink:
-      "https://www.instagram.com/cops.iitbhu?igsh=MXRvdjBpMWRkdmRkeA==",
+      'https://www.instagram.com/cops.iitbhu?igsh=MXRvdjBpMWRkdmRkeA==',
   },
   {
     mainImage: tqc,
-    clubName: "The Quant Club",
+    clubName: 'The Quant Club',
     content:
-      "The Quant Club at IIT (BHU), Varanasi, promotes quantitative finance and analytics through workshops, seminars, competitions, and projects. We offer learning, networking, and hands-on experience in algorithmic trading, risk management, and data analytics. Join us to excel in this field.",
-    color: "purple",
-    whatsappLink: "https://chat.whatsapp.com/JwehHeWwlc80n8OVXZy4LL",
+      'Promotes quantitative finance and analytics through workshops, seminars, competitions, and projects. Join us to excel in algorithmic trading and data analytics.',
+    color: 'purple',
+    whatsappLink: 'https://chat.whatsapp.com/JwehHeWwlc80n8OVXZy4LL',
     linkedinLink:
-      "https://www.linkedin.com/company/the-quant-club-iit-bhu-varanasi/",
+      'https://www.linkedin.com/company/the-quant-club-iit-bhu-varanasi/',
     instaLink:
-      "https://www.instagram.com/quantclub.iitbhu?igsh=eDIzaWd2YzM0dWpi",
+      'https://www.instagram.com/quantclub.iitbhu?igsh=eDIzaWd2YzM0dWpi',
   },
   {
     mainImage: robotics,
-    clubName: "Robotics Club",
+    clubName: 'Robotics Club',
     content:
-      "Our Robotics Club at IIT BHU empowers students with essential skills in robotics through hands-on projects, workshops, guest lectures, and competitions. Whether you're a beginner or expert, join us to learn, grow, and launch a successful robotics career.",
-    color: "skyBlue",
-    whatsappLink: "https://chat.whatsapp.com/CSGHZY6xmlCIQEro82i1tz",
+      'Empowers students with essential robotics skills through projects, workshops, lectures, and competitions. Join us to learn and launch a successful robotics career.',
+    color: 'skyBlue',
+    whatsappLink: 'https://chat.whatsapp.com/CSGHZY6xmlCIQEro82i1tz',
     linkedinLink:
-      "https://www.linkedin.com/company/robotics-club-iit-bhu-varanasi/",
+      'https://www.linkedin.com/company/robotics-club-iit-bhu-varanasi/',
     instaLink:
-      "https://www.instagram.com/robotics_club.iitbhu?igsh=MXFhYTdtZms0MnZscQ==",
+      'https://www.instagram.com/robotics_club.iitbhu?igsh=MXFhYTdtZms0MnZscQ==',
   },
   {
     mainImage: aero,
-    clubName: "Aero-Modelling Club",
+    clubName: 'Aero-Modelling Club',
     content:
-      "The Aeromodelling Club at IIT (BHU) fosters creativity and innovation in aviation. We provide hands-on experience in designing, building, and flying model aircraft through workshops, competitions, and demonstrations. Join us to explore remote-controlled planes, drones, and rocketry, and soar to new heights.",
-    color: "red",
-    whatsappLink: "https://chat.whatsapp.com/GYcADtLYmqZHnuqDF34HCq",
-    linkedinLink: "https://www.linkedin.com/company/amc-rocks/",
-    instaLink: "https://www.instagram.com/amc.iitbhu?igsh=aTJsdDR2YzkxcXJt",
+      'Fosters creativity in aviation with hands-on experience in designing, building, and flying model aircraft. Join us to explore planes, drones, and rocketry.',
+    color: 'red',
+    whatsappLink: 'https://chat.whatsapp.com/GYcADtLYmqZHnuqDF34HCq',
+    linkedinLink: 'https://www.linkedin.com/company/amc-rocks/',
+    instaLink: 'https://www.instagram.com/amc.iitbhu?igsh=aTJsdDR2YzkxcXJt',
   },
   {
     mainImage: csi,
-    clubName: "Club of Sustainability and Innovation",
+    clubName: 'Club of Sustainability and Innovation',
     content:
-      "The Club Of Sustainability and Innovation at IIT (BHU) drives positive change through sustainable practices and innovative solutions. We raise awareness and implement projects in environmental conservation, social responsibility, and technology. Join us to turn ideas into action and create a greener future.",
-    color: "orange",
-    whatsappLink: "https://chat.whatsapp.com/BPybNc2nPE6KS1AzdjMrWq",
-    linkedinLink: "https://www.linkedin.com/company/csi-iitbhu/",
-    instaLink: "https://www.instagram.com/csi_iitbhu?igsh=MXBnYWxmdnlzenZ2dQ==",
+      'Drives change through sustainable practices and innovative solutions. Join us to turn ideas into action for a greener future.',
+    color: 'orange',
+    whatsappLink: 'https://chat.whatsapp.com/BPybNc2nPE6KS1AzdjMrWq',
+    linkedinLink: 'https://www.linkedin.com/company/csi-iitbhu/',
+    instaLink: 'https://www.instagram.com/csi_iitbhu?igsh=MXBnYWxmdnlzenZ2dQ==',
   },
   {
     mainImage: bizclub,
-    clubName: "The Business Club",
+    clubName: 'The Business Club',
     content:
-      "Join the Business Club at IIT BHU to gain essential skills for success in the business world. We offer guest lectures, hands-on projects, and workshops to enhance your understanding of business and prepare you for a competitive global marketplace.",
-    color: "red",
-    whatsappLink: "https://chat.whatsapp.com/Iix3yxoYqUM0kMkQun9M4s",
-    linkedinLink: "https://www.linkedin.com/company/businessclubiitbhu/",
+      'Gain essential business skills through guest lectures, projects, and workshops. Join us to prepare for a competitive global marketplace.',
+    color: 'red',
+    whatsappLink: 'https://chat.whatsapp.com/Iix3yxoYqUM0kMkQun9M4s',
+    linkedinLink: 'https://www.linkedin.com/company/businessclubiitbhu/',
     instaLink:
-      "https://www.instagram.com/businessclub_iitbhu?igsh=Mmg3eGZldW1ia2Rs",
+      'https://www.instagram.com/businessclub_iitbhu?igsh=Mmg3eGZldW1ia2Rs',
   },
   {
     mainImage: astro,
-    clubName: "Astronomy Club",
+    clubName: 'Astronomy Club',
     content:
-      "The Astronomy Club at IIT (BHU) explores the cosmos through stargazing, workshops, and outreach. We ignite curiosity and inspire awe about celestial phenomena, astrophotography, and space exploration. Join us to witness cosmic events and deepen your understanding of the universe.",
-    color: "blue",
-    whatsappLink: "https://chat.whatsapp.com/KLyly1vfE0f8JUpg7lgSZb",
-    linkedinLink: "https://www.linkedin.com/company/astroiitbhu/",
-    instaLink: "https://www.instagram.com/astro.iitbhu?igsh=a2s0dGc5aG9naTh2",
+      'Explores the cosmos through stargazing, workshops, and outreach. Join us to witness cosmic events and deepen your understanding of the universe.',
+    color: 'blue',
+    whatsappLink: 'https://chat.whatsapp.com/KLyly1vfE0f8JUpg7lgSZb',
+    linkedinLink: 'https://www.linkedin.com/company/astroiitbhu/',
+    instaLink: 'https://www.instagram.com/astro.iitbhu?igsh=a2s0dGc5aG9naTh2',
   },
   {
     mainImage: sae,
-    clubName: "Society of Automotive Engineers",
+    clubName: 'Society of Automotive Engineers',
     content:
-      "The Society of Automotive Engineering at IIT (BHU) explores cutting-edge automotive technologies. We focus on science, engineering, and design, from conventional to electric and autonomous systems. Join us for workshops, seminars, and projects to innovate in automotive technology and drive mobility's future.",
-    color: "red",
-    whatsappLink: "https://chat.whatsapp.com/LKSYDe8W2jY3NFvChJq2ow",
+      'Explores automotive technologies with a focus on engineering and design. Join us for workshops and projects to innovate in automotive technology.',
+    color: 'red',
+    whatsappLink: 'https://chat.whatsapp.com/LKSYDe8W2jY3NFvChJq2ow',
     linkedinLink:
-      "https://www.linkedin.com/company/sae-collegiate-club-iit-varanasi/",
+      'https://www.linkedin.com/company/sae-collegiate-club-iit-varanasi/',
     instaLink:
-      "https://www.instagram.com/saecollegiateclubiitbhu?igsh=MXMzYjNzNWd0ZG5oZw==",
+      'https://www.instagram.com/saecollegiateclubiitbhu?igsh=MXMzYjNzNWd0ZG5oZw==',
   },
-];
+]
 
 function App() {
-  const [currentIndex, setCurrentIndex] = useState<number>(db.length - 1);
-  const currentIndexRef = useRef<number>(currentIndex);
+  const [currentIndex, setCurrentIndex] = useState<number>(db.length - 1)
+  const currentIndexRef = useRef<number>(currentIndex)
 
   const childRefs: RefObject<TinderCardAPI>[] = useMemo(
     () =>
       Array(db.length)
         .fill(null)
         .map(() => React.createRef<TinderCardAPI>()),
-    [db.length]
-  );
+    []
+  )
 
   const updateCurrentIndex = (val: number) => {
-    setCurrentIndex(val);
-    currentIndexRef.current = val;
-  };
+    setCurrentIndex(val)
+    currentIndexRef.current = val
+  }
 
-  const canGoBack = currentIndex < db.length - 1;
+  const canGoBack = currentIndex < db.length - 1
 
   const swiped = (direction: Direction, index: number) => {
-    updateCurrentIndex(index - 1);
-    console.log("Swiped: ", direction, index);
-  };
+    updateCurrentIndex(index - 1)
+    console.log('Swiped: ', direction, index)
+  }
 
   const outOfFrame = (idx: number) => {
     if (currentIndexRef.current >= idx) {
-      childRefs[idx].current?.restoreCard();
+      childRefs[idx].current?.restoreCard()
     }
-  };
+  }
 
   const goBack = async () => {
-    if (!canGoBack) return;
-    const newIndex = currentIndex + 1;
-    updateCurrentIndex(newIndex);
-    await childRefs[newIndex].current?.restoreCard();
-  };
+    if (!canGoBack) return
+    const newIndex = currentIndex + 1
+    updateCurrentIndex(newIndex)
+    await childRefs[newIndex].current?.restoreCard()
+  }
 
   return (
     <>
@@ -177,24 +178,28 @@ function App() {
         <SignInPage />
       </SignedOut>
       <SignedIn>
-      <div className="relative w-screen h-screen bg-black flex flex-col items-center">
+        <div className='relative w-screen h-screen bg-black flex flex-col items-center'>
           <link
-            href="https://fonts.googleapis.com/css?family=Damion&display=swap"
-            rel="stylesheet"
+            href='https://fonts.googleapis.com/css?family=Damion&display=swap'
+            rel='stylesheet'
           />
-          <div className="absolute top-5 right-5">
-            <UserButton />
-          </div>
-          <div className="flex flex-col items-center mt-16 w-[90vw] max-w-[600px] h-[80vh]">
-            <h1 className="text-3xl font-bold text-white mb-8 bg-gradient-to-r from-white via-gray-300 to-gray-200 bg-clip-text text-transparent p-2">
+
+          <div className='flex flex-row justify-around absolute top-5 w-full'>
+            <h1 className='text-3xl font-bold text-white bg-clip-text text-transparent p-2'>
               SNTC Induction 2024
             </h1>
-            <div className="relative w-full h-full flex items-center justify-center">
+            <UserButton />
+          </div>
+          <div className='flex flex-col items-center mt-16 w-[90vw] max-w-[600px] h-[80vh]'>
+            {/* <h1 className='text-3xl font-bold text-white bg-clip-text text-transparent p-2'>
+              SNTC Induction 2024
+            </h1> */}
+            <div className='relative w-full h-full flex items-center justify-center'>
               {db.map((club, index) => (
                 <TinderCard
                   ref={childRefs[index]}
                   className={`absolute w-full h-full flex items-center justify-center transition-transform duration-100 ${
-                    index === currentIndex ? "z-10" : ""
+                    index === currentIndex ? 'z-10' : ''
                   }`}
                   key={club.clubName}
                   onSwipe={(dir) => swiped(dir as Direction, index)}
@@ -213,10 +218,10 @@ function App() {
                 </TinderCard>
               ))}
             </div>
-            <div className="fixed bottom-5 w-full flex justify-center">
+            <div className='fixed bottom-5 w-full flex justify-center'>
               <button
                 onClick={() => goBack()}
-                className="bg-gradient-to-r from-pink-300 via-pink-200 to-orange-300 rounded-full p-3 shadow-lg hover:scale-110 transition-transform duration-300 ease-in-out hover:shadow-xl active:bg-white flex items-center justify-center w-15 h-15"
+                className='bg-gradient-to-r from-pink-300 via-pink-200 to-orange-300 rounded-full p-3 shadow-lg hover:scale-110 transition-transform duration-300 ease-in-out hover:shadow-xl active:bg-white flex items-center justify-center w-15 h-15'
               >
                 <FaUndo size={20} />
               </button>
@@ -225,7 +230,7 @@ function App() {
         </div>
       </SignedIn>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { dark } from '@clerk/themes'
+import './index.css'
+import { ClerkProvider } from '@clerk/clerk-react'
+import App from './App'
 
-import "./index.css";
-import { ClerkProvider } from "@clerk/clerk-react";
-import App from "./App";
-
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY as string;
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY as string
 
 if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key");
+  throw new Error('Missing Publishable Key')
 }
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+    <ClerkProvider
+      publishableKey={PUBLISHABLE_KEY}
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
       <App />
     </ClerkProvider>
-  </React.StrictMode>,
-);
+  </React.StrictMode>
+)
