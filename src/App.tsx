ecfc77a32c6ecc70f042afdb55cf08136a4f7708
Divyansh Seth <divyanshseth08@gmail.com>
2024-07-30 13:52:49 +0530

    fixed some colors and size of cards for small screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import './App.css';
-import CopsCard from './components/cops';
-import cops from './assets/cops.svg';
-import aero from './assets/aero.svg';
-import tqc from './assets/tqc.svg';
-import robotics from './assets/robotics.svg';
-import csi from './assets/csi.svg';
-import bizclub from './assets/bizclub.svg';
-import astro from './assets/astro.svg';
-
-
+import './App.css'
+import CopsCard from './components/cops'
+import cops from './assets/cops.svg'
+import aero from './assets/aero.svg'
+import tqc from './assets/tqc.svg'
+import robotics from './assets/robotics.svg'
+import csi from './assets/csi.svg'
+import bizclub from './assets/bizclub.svg'
+import astro from './assets/astro.svg'
+import sae from './assets/sae.svg'
 
 function App() {
   return (
@@ -16,7 +15,7 @@ function App() {
       <CopsCard
         mainImage={cops}
         clubName='Club Of Programmers'
-        content="COPS at IIT (BHU), Varanasi, fosters programming excellence through workshops, competitions, hackathons, and projects. We empower members with skills and knowledge, nurturing creativity and problem-solving. Join us to grow, connect, and excel in technology."
+        content='COPS at IIT (BHU), Varanasi, fosters programming excellence through workshops, competitions, hackathons, and projects. We empower members with skills and knowledge, nurturing creativity and problem-solving. Join us to grow, connect, and excel in technology.'
         color='black'
         whatsappLink=''
         linkedinLink='https://www.linkedin.com/company/cops-iitbhu/'
@@ -25,7 +24,7 @@ function App() {
       <CopsCard
         mainImage={tqc}
         clubName='The Quant Club'
-        content="The Quant Club at IIT (BHU), Varanasi, promotes quantitative finance and analytics through workshops, seminars, competitions, and projects. We offer learning, networking, and hands-on experience in algorithmic trading, risk management, and data analytics. Join us to excel in this field."
+        content='The Quant Club at IIT (BHU), Varanasi, promotes quantitative finance and analytics through workshops, seminars, competitions, and projects. We offer learning, networking, and hands-on experience in algorithmic trading, risk management, and data analytics. Join us to excel in this field.'
         color='purple'
         whatsappLink='https://chat.whatsapp.com/JwehHeWwlc80n8OVXZy4LL'
         linkedinLink='https://www.linkedin.com/company/the-quant-club-iit-bhu-varanasi/'
@@ -41,7 +40,7 @@ function App() {
         instaLink='https://www.instagram.com/robotics_club.iitbhu?igsh=MXFhYTdtZms0MnZscQ=='
       />
       <CopsCard
-        mainImage={robotics}
+        mainImage={sae}
         clubName='Society of Automotive Engineers'
         content="The Society of Automotive Engineering at IIT (BHU) explores cutting-edge automotive technologies. We focus on science, engineering, and design, from conventional to electric and autonomous systems. Join us for workshops, seminars, and projects to innovate in automotive technology and drive mobility's future."
         color='red'
@@ -52,7 +51,7 @@ function App() {
       <CopsCard
         mainImage={aero}
         clubName='Aero-Modelling Club'
-        content="The Aeromodelling Club at IIT (BHU) fosters creativity and innovation in aviation. We provide hands-on experience in designing, building, and flying model aircraft through workshops, competitions, and demonstrations. Join us to explore remote-controlled planes, drones, and rocketry, and soar to new heights."
+        content='The Aeromodelling Club at IIT (BHU) fosters creativity and innovation in aviation. We provide hands-on experience in designing, building, and flying model aircraft through workshops, competitions, and demonstrations. Join us to explore remote-controlled planes, drones, and rocketry, and soar to new heights.'
         color='red'
         whatsappLink='https://chat.whatsapp.com/GYcADtLYmqZHnuqDF34HCq'
         linkedinLink='https://www.linkedin.com/company/amc-rocks/'
@@ -61,7 +60,7 @@ function App() {
       <CopsCard
         mainImage={csi}
         clubName='Club of Sustainability and Innovation'
-        content="The Club Of Sustainability and Innovation at IIT (BHU) drives positive change through sustainable practices and innovative solutions. We raise awareness and implement projects in environmental conservation, social responsibility, and technology. Join us to turn ideas into action and create a greener future."
+        content='The Club Of Sustainability and Innovation at IIT (BHU) drives positive change through sustainable practices and innovative solutions. We raise awareness and implement projects in environmental conservation, social responsibility, and technology. Join us to turn ideas into action and create a greener future.'
         color='orange'
         whatsappLink='https://chat.whatsapp.com/BPybNc2nPE6KS1AzdjMrWq'
         linkedinLink='https://www.linkedin.com/company/csi-iitbhu/'
@@ -70,8 +69,8 @@ function App() {
       <CopsCard
         mainImage={bizclub}
         clubName='The Business Club'
-        content="Join the Business Club at IIT BHU to gain essential skills for success in the business world. We offer guest lectures, hands-on projects, and workshops to enhance your understanding of business and prepare you for a competitive global marketplace."
-        color='blue'
+        content='Join the Business Club at IIT BHU to gain essential skills for success in the business world. We offer guest lectures, hands-on projects, and workshops to enhance your understanding of business and prepare you for a competitive global marketplace.'
+        color='red'
         whatsappLink='https://chat.whatsapp.com/Iix3yxoYqUM0kMkQun9M4s'
         linkedinLink='https://www.linkedin.com/company/businessclubiitbhu/'
         instaLink='https://www.instagram.com/businessclub_iitbhu?igsh=Mmg3eGZldW1ia2Rs'
@@ -79,14 +78,14 @@ function App() {
       <CopsCard
         mainImage={astro}
         clubName='Astronomy Club'
-        content="The Astronomy Club at IIT (BHU) explores the cosmos through stargazing, workshops, and outreach. We ignite curiosity and inspire awe about celestial phenomena, astrophotography, and space exploration. Join us to witness cosmic events and deepen your understanding of the universe."
+        content='The Astronomy Club at IIT (BHU) explores the cosmos through stargazing, workshops, and outreach. We ignite curiosity and inspire awe about celestial phenomena, astrophotography, and space exploration. Join us to witness cosmic events and deepen your understanding of the universe.'
         color='red'
         whatsappLink='https://chat.whatsapp.com/KLyly1vfE0f8JUpg7lgSZb'
         linkedinLink='https://www.linkedin.com/company/astroiitbhu/'
         instaLink='https://www.instagram.com/astro.iitbhu?igsh=a2s0dGc5aG9naTh2'
       />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/cops.tsx
+++ src/components/cops.tsx
@@ -1,41 +1,47 @@
-import React from 'react';
-import { FaInstagram, FaLinkedin, FaGithub, FaEnvelope, FaWhatsapp } from 'react-icons/fa';
-import sntc from '../assets/sntc.png';
+import React from 'react'
+import {
+  FaInstagram,
+  FaLinkedin,
+  FaGithub,
+  FaEnvelope,
+  FaWhatsapp,
+} from 'react-icons/fa'
+import sntc from '../assets/sntc.png'
 
 const colorVariants = {
-  black: 'text-gray-800 hover:text-gray-900',
+  black: 'text-[#1F2C33] hover:text-gray-900',
   blue: 'text-blue-800 hover:text-blue-900',
-  orange: 'text-orange-500 hover:text-orange-700',
-  purple: 'text-purple-800 hover:text-purple-900',
-  red: 'text-red-800 hover:text-red-900',
-  skyBlue: 'text-sky-500 hover:text-sky-700',
+  orange: 'text-[#FF5A1F] hover:text-orange-700',
+  purple: 'text-[#5A0096] hover:text-purple-700',
+  red: 'text-[#D23337] hover:text-red-800',
+  skyBlue: 'text-[#0C76BE] hover:text-sky-700',
   lightPurple: 'text-purple-500 hover:text-purple-700',
   lightRed: 'text-red-500 hover:text-red-700',
   darkPurple: 'text-purple-900 hover:text-purple-800',
   darkRed: 'text-red-900 hover:text-red-800',
-};
+}
 
 const colorVariantsWhatsapp = {
-  black: 'bg-gray-800 active:bg-gray-900',
+  black: 'bg-[#1F2C33] active:bg-gray-900',
   blue: 'bg-blue-800 active:bg-blue-900',
-  orange: 'bg-orange-500 active:bg-orange-700',
-  purple: 'bg-purple-800 active:bg-purple-900',
-  red: 'bg-red-800 active:bg-red-900',
-  skyBlue: 'bg-sky-500 active:bg-sky-700',
+  orange: 'bg-[#FF5A1F] active:bg-orange-700',
+  purple: 'bg-[#5A0096] active:bg-purple-700',
+  red: 'bg-[#D23337] active:bg-red-800',
+  skyBlue: 'bg-[#0B8DC3] active:bg-[#0C76BE]',
   lightPurple: 'bg-purple-500 active:bg-purple-700',
   lightRed: 'bg-red-500 active:bg-red-700',
   darkPurple: 'bg-purple-900 active:bg-purple-800',
   darkRed: 'bg-red-900 active:bg-red-800',
-};
+}
 
 interface CopsCardProps {
-  mainImage: string;
-  clubName: string;
-  content: string;
-  color: keyof typeof colorVariants;
-  whatsappLink: string;
-  instaLink: string;
-  linkedinLink: string;
+  mainImage: string
+  clubName: string
+  content: string
+  color: keyof typeof colorVariants
+  whatsappLink: string
+  instaLink: string
+  linkedinLink: string
 }
 
 const CopsCard: React.FC<CopsCardProps> = ({
@@ -48,40 +54,43 @@ const CopsCard: React.FC<CopsCardProps> = ({
   linkedinLink,
 }) => {
   return (
-    <div className="bg-white p-8 rounded-3xl shadow-lg text-center flex flex-col justify-center align-middle">
-      <div className="flex justify-start mb-4">
-        <img src={sntc} alt="SNTC" className="rounded-full w-10" />
+    <div className='bg-white pt-4 pl-4 pb-8 pr-4 rounded-3xl shadow-lg text-center flex flex-col justify-center align-middle'>
+      <div className='flex justify-start'>
+        <img src={sntc} alt='SNTC' className='rounded-full w-10 md:w-' />
       </div>
-      <div className="flex justify-center mb-4">
-        <img src={mainImage} alt="COPS" className="w-1/2 rounded-full border" />
+      <div className='flex justify-center mt-[-2rem] mb-[-0.5rem]'>
+        <img src={mainImage} alt='COPS' className='w-[12rem] rounded-full' />
       </div>
-      <h1 className="text-xl font-bold mb-4">{clubName}</h1>
-      <p className="text-gray-700 mb-4">{content}</p>
-      <div className="flex justify-center align-middle">
+      <h1 className='text-xl font-bold mb-2'>{clubName}</h1>
+      <p className='text-gray-700 mb-4'>{content}</p>
+      <div className='flex justify-center align-middle'>
         <button
           className={`${colorVariantsWhatsapp[color]} rounded-3xl text-white flex justify-center align-middle p-2 my-3 w-1/2 font-semibold`}
           onClick={() => window.open(whatsappLink, '_blank')}
         >
-          <FaWhatsapp size={24} className="mr-2" />
+          <FaWhatsapp size={24} className='mr-2' />
           WhatsApp
         </button>
       </div>
-      <div className="flex justify-center space-x-4 mt-1">
+      <div className='flex justify-center space-x-4 mt-1'>
         <a href={instaLink} className={`${colorVariants[color]}`}>
           <FaInstagram size={24} />
         </a>
         <a href={linkedinLink} className={`${colorVariants[color]}`}>
           <FaLinkedin size={24} />
         </a>
-        <a href="https://github.com" className={`${colorVariants[color]}`}>
+        <a href='https://github.com' className={`${colorVariants[color]}`}>
           <FaGithub size={24} />
         </a>
-        <a href="mailto:example@example.com" className={`${colorVariants[color]}`}>
+        <a
+          href='mailto:example@example.com'
+          className={`${colorVariants[color]}`}
+        >
           <FaEnvelope size={24} />
         </a>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default CopsCard;
+export default CopsCard
